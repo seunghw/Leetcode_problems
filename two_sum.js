153c0210741150c38c9e7bc01ccff6deb139a094
Seunghwan <ysp1307@naver.com>
2021-12-22 22:13:49 +0900

    best sol o

---
+++ two_sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+const twoSum = (nums, target) => {
+  const data = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const another = target - nums[i];
+
+    if (another in data) {
+      return [data[another], i];
+    }
+
+    data[nums[i]] = i;
+  }
+
+  return null;
+};
