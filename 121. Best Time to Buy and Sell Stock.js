4848d390f4be542edfa297acd705c5e26ae73d05
Seunghwan <ysp1307@naver.com>
2022-01-15 15:31:45 +0900

    mysol myop

---
+++ 121. Best Time to Buy and Sell Stock.js
@@ -0,0 +1,12 @@
+var maxProfit = function (prices) {
+  let min = prices[0];
+  let max = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    min = Math.min(min, prices[i]);
+    max = Math.max(max, prices[i] - min);
+  }
+  return max;
+};
+
+// 최대값과 최소값을 바교해서 반환
