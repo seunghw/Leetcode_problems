7ea3200761927f6067b45c622b2fef39c93a2d2e
Seunghwan <ysp1307@naver.com>
2022-01-19 15:23:29 +0900

    mysol myop

---
+++ 2037. Minimum Number of Moves to Seat Everyone.js
@@ -0,0 +1,9 @@
+var minMovesToSeat = function (seats, students) {
+  let count = 0;
+  seats.sort((x, y) => x - y);
+  students.sort((x, y) => x - y);
+  for (let i = 0; i < seats.length; i++) {
+    count += Math.abs(seats[i] - students[i]);
+  }
+  return count;
+};
